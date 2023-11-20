3cdbbc012ca7c1b8bfae822e2f79a1ae6e879ac7
GarrisonINC <111631791+GarrisonINC@users.noreply.github.com>
2023-11-20 20:13:01 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function setup() {
+    canvas=createCanvas(400,450);
+    canvas.position(650,70);
+    video=createCapture(200,200);
+
+
+}
+
+function draw() {
+background("grey");
+fill("red");
+rect(200,200,120,80);
+
+}
